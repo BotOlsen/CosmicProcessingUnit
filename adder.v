f3e916147010ecc0283104a9e15b9b984c5a4c7c
Daniel Olsen <dolsen@csus.edu>
2021-04-25 18:14:57 -0700

    Wrote adder.v and adder_fixture.v

--- adder.v
+++ adder.v
@@ -1,1 +1,16 @@
+/*
+ * Date: 4-25-2021
+ * Author: Daniel Olsen
+ * Name: adder
+ */
 
+module adder 
+(
+    input signed [15:0] A, B, 
+    output reg signed [15:0] sum
+);
+    always @(*)
+    begin
+        sum = A + B;
+    end
+endmodule
---
+++ adder_fixture.v
@@ -0,0 +1,45 @@
+/*
+ * Date: 4-25-2021
+ * Author: Daniel Olsen
+ * Name: adder_fixture
+ */
+
+`include "adder.v"
+
+module adder_fixture;
+
+reg [15:0] op1;
+reg [15:0] op2;
+wire [15:0] R;
+
+initial
+	$vcdpluson;
+
+initial
+	$monitor($time, "  A = %h B = %h     A + B = %h  ", op1[15:0], op2[15:0], R[15:0]);
+
+adder adderInstance(.A(op1), .B(op2), .sum(R));
+
+//Op1 inputs change every 15 time units
+initial
+begin
+	op1 = 16'h0021;
+	#15 op1 = 16'h7676;
+	#15 op1 = 16'h1234;
+end
+
+//Op2 inputs change every 15 time units
+initial
+begin
+	op2 = 16'hF0FF;
+	#15 op2 = 16'h0321;
+	#15 op2 = 16'h6998;
+end
+
+//finish at 210
+initial
+begin
+	#60 $finish;
+end
+
+endmodule
