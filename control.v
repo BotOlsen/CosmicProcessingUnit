4200220e3dcad5793ccff6e9f87df6b5956912f7
Daniel Olsen <dolsen@csus.edu>
2021-05-09 19:40:44 -0700

    fixed ALUControlOp for OR Imme and Comparator bits

--- control.v
+++ control.v
@@ -9,8 +9,8 @@ module control
 (
     input multiDiv,
     input [3:0] opcode,
-    output reg aluBType, aluSrc, signChange, memRead, memToReg, memWrite,
-    output reg [1:0] aluControl, regWrite, jumpBranch
+    output reg aluBType, aluSrc, signExtend, memRead, memToReg, memWrite,
+    output reg [1:0] aluControlOp, regWrite, jumpBranch
 );
 
 always@(*)
@@ -21,12 +21,12 @@ begin
         begin
            aluBType = 1'b0;
            aluSrc = 1'b0;
-           aluControl = 2'b00;
-           signChange = 1'b0;
+           aluControlOp = 2'b00;
+           signExtend = 1'b0;
            memRead = 1'b0;
            memToReg = 1'b0;
            memWrite = 1'b0;
-           jumpBranch = 2'b00;
+           jumpBranch = 3'b000;
             if(multiDiv) // if type A AND appears to be multi/division
                  regWrite = 2'b11;
             else // add/sub
@@ -37,143 +37,143 @@ begin
         begin
             aluBType = 1'b0;
             aluSrc = 1'b1;
-            aluControl = 2'b01;
+            aluControlOp = 2'b01;
             regWrite = 2'b01;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b0;
             memToReg = 1'b0;
             memWrite = 1'b0;
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
         
         4'b0010: // OR immediate type C
         begin
             aluBType = 1'b0;
             aluSrc = 1'b1;
-            aluControl = 2'b01;
+            aluControlOp = 2'b11;
             regWrite = 2'b01;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b0;
             memToReg = 1'b0;
             memWrite = 1'b0;
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
 
         4'b1010: // LBU type B
         begin
             aluBType = 1'b1;
             aluSrc = 1'b1;
-            aluControl = 2'b10;
+            aluControlOp = 2'b10;
             regWrite = 2'b00;
-            signChange = 1'b1;
+            signExtend = 1'b1;
             memRead = 1'b1;
             memToReg = 1'b1;
             memWrite = 1'b0;
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
 
         4'b1011: // Store Byte type B
         begin
             aluBType = 1'b1;
             aluSrc = 1'b1;
-            aluControl = 2'b10;
+            aluControlOp = 2'b10;
             regWrite = 2'b01;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b0;
             memToReg = 1'bx; // dont care (please check this)
             memWrite = 1'b1; 
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
         
         4'b1100: // load type B 
         begin
             aluBType = 1'b1;
             aluSrc = 1'b1;
-            aluControl = 2'b10;
+            aluControlOp = 2'b10;
             regWrite = 2'b00;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b1;
             memToReg = 1'b1;
             memWrite = 1'b0; 
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
 
         4'b1101: // store type B
         begin
             aluBType = 1'b1;
             aluSrc = 1'b1;
-            aluControl = 2'b10;
+            aluControlOp = 2'b10;
             regWrite = 2'b01;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b0;
             memToReg = 1'bx; // dont care (please check this)
             memWrite = 1'b1; 
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
 
         4'b0101: // branch on less than type C
         begin
             aluBType = 1'bx; // dont care (please check this)
             aluSrc = 1'bx; // ^ 
-            aluControl = 2'bxx; // ^ 
+            aluControlOp = 2'bxx; // ^ 
             regWrite = 2'b00; 
-            signChange = 1'bx;
+            signExtend = 1'bx;
             memRead = 1'b0;
             memToReg = 1'bx;
             memWrite = 1'b0;
-            jumpBranch = 2'b01;
+            jumpBranch = 3'b001;
         end
 
         4'b0100: // branch greater than type C 
         begin
             aluBType = 1'bx;
             aluSrc = 1'bx;
-            aluControl = 2'bxx;
+            aluControlOp = 2'bxx;
             regWrite = 2'b00;
-            signChange = 1'bx;
+            signExtend = 1'bx;
             memRead = 1'b0;
             memToReg = 1'bx;
             memWrite = 1'b0;
-            jumpBranch = 2'b01;
+            jumpBranch = 3'b010;
         end
 
         4'b0110: // branch equal type C
         begin
            aluBType = 1'bx;
            aluSrc = 1'bx;
-           aluControl = 2'bxx;
+           aluControlOp = 2'bxx;
            regWrite = 2'b00;
-           signChange = 1'bx;
+           signExtend = 1'bx;
            memRead = 1'b0;
            memToReg = 1'bx;
            memWrite = 1'b0;
-           jumpBranch = 2'b01;
+           jumpBranch = 3'b011;
         end
         
         4'b0111: // jump type D
         begin
              aluBType = 1'bx;
              aluSrc = 1'bx;
-             aluControl = 2'bxx;
+             aluControlOp = 2'bxx;
              regWrite = 2'b00;
-             signChange = 1'bx;
+             signExtend = 1'bx;
              memRead = 1'b0;
              memToReg = 1'bx;
              memWrite = 1'b0;
-             jumpBranch = 2'b11;
+             jumpBranch = 2'b100;
         end
 
         4'b0000: // halt type D
         begin
             aluBType = 1'b0;
             aluSrc = 1'b0;
-            aluControl = 2'b00;
+            aluControlOp = 2'b00;
             regWrite = 2'b00;
-            signChange = 1'b0;
+            signExtend = 1'b0;
             memRead = 1'b0;
             memToReg = 1'b0;
             memWrite = 1'b0;
-            jumpBranch = 2'b00;
+            jumpBranch = 3'b000;;
         end
     endcase
   end
