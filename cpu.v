abe9d27f8e80d5555851a58c61001bba1aa329b7
Daniel Olsen <dolsen@csus.edu>
2021-05-12 05:44:26 -0700

    all modules added besides FU and HDU.

--- cpu.v
+++ cpu.v
@@ -15,6 +15,10 @@
 `include "sign_extend.v"
 `include "shiftLeft.v"
 `include "control.v"
+`include "alu.v"
+`include "alu_control.v"
+`include "dataMemory.v"
+`include "zero_extend.v"
 
 module cpu(
     input clk, reset_n, 
@@ -23,11 +27,16 @@ module cpu(
     output [2:0] jumpBranch,
     output [15:0] PCOutput, IFAdderOutput, Instruction, rd1Read, rd2Read, reg0Read, signExtendedImmediate, ShiftResult, IDAdderOutput,
     output [31:0] IFID_Output /*[31:16] IFAdderOutput, [15:0] Instruction Output*/
-    output [61:0] IDEX_Output /*[3:0] Function Code, [19:4] signExtendedImmediate, [35:20] rd2SrcMuxOutput, [51:36]rd1SrcMuxOutput, [52] ALUBType, [53] ALUSrc, [55:54] ALUControlOp}
-                                [56] MemRead, [57] MemWrite, [58] zeroExtendFlag, [59] memToReg, [60:61] RegWrite*/
+    output [65:0] IDEX_Output, /*[3:0] Function Code, [19:4] signExtendedImmediate, [35:20] rd2SrcMuxOutput, [51:36]rd1SrcMuxOutput, [52] ALUBType, [53] ALUSrc, [55:54] ALUControlOp}
+                                [56] MemRead, [57] MemWrite, [58] zeroExtendFlag, [59] memToReg, [60:61] RegWrite, [65:62] EXRegDst*/
+    output [57:0] EXM_Output, /*[3:0] MRegDest, [19:4] DataToWrite, [35:20] ALUOverflowOutput, [51:36] ALUOutput, [52] MemRead, [53] MemWrite, [54] zeroExtendFlag, [55] memToReg, [57:56] RegWrite*/
+    output [55:0] MWB_Output /* [3:0] WBRegDest, [19:4] ALUOverflowOutput, [35:20] DataMemoryOutputMuxResult, [51:36] ALUOutput, [52] memToReg, [54:53]RegWrite*/
 );
 
-wire [15:0] PCSourceMuxOutput;
+.in({ EXM[57:56], EXM[55], EXM[51:36], DataMemoryOutputMuxResult, EXM[35:20], EXM[3:0]}),
+
+
+wire [15:0] PCSourceMuxOutput, ALUSource1MuxOutput, ALUSource2MuxOutput, exReg0, ALUOverflowOutput, ALUOutput, DataMemoryOutput, zeroExtendedResult, MemToRegMuxOutput;
 wire [9:0] SignalFlushMuxOutput;
 
 //Instantiation of IF Stage
@@ -85,7 +94,7 @@ register RegisterFile(
 mux4to1 Register0SourceMux(
         .switch(2'b00),                      //Add later, output of OFrwarding UNnit (R0Fwd)
         .input1(reg0Read),
-        .input2(16'h0000),                  //Add later. EX STAGE R OUTPUT
+        .input2(ALUOverflowOutput),  
         .input3(16'h0000),                  //Add later. m Stage R OUTPUT
         .input4(16'h0000),                   //Add later, WB STAGE R OUTPUT
         .out(reg0Read)
@@ -94,7 +103,7 @@ mux4to1 Register0SourceMux(
 mux4to1 rd1SourceMux(
         .switch(2'b00),                      //Add later, output of OFrwarding UNnit (Op1Fwd)
         .input1(reg0Read),
-        .input2(16'h0000),                  //Add later. EX STAGE ALU OUTPUT
+        .input2(ALUOutput), 
         .input3(16'h0000),                  //Add later. m Stage ALU OUTPUT
         .input4(16'h0000)                   //Add later, WB STAGE ALU OUTPUT
 );
@@ -152,9 +161,9 @@ mux2to1 #(.SIZE(10)) SignalFlushMux (
 );
 
 /*[3:0] Function Code, [19:4] signExtendedImmediate, [35:20] rd2SrcMuxOutput, [51:36]rd1SrcMuxOutput, [52] ALUBType, [53] ALUSrc, [55:54] ALUControlOp}
-                                [56] MemRead, [57] MemWrite, [58] zeroExtendFlag, [59] memToReg, [61:60] RegWrite*/
-stage_buffer #(.SIZE(62)) IDEX (
-        .in({SignalFlushMuxOutput, rd1SrcMuxOutput, rd2SrcMuxOutput, signExtendedImmediate, IFID_Output[3:0]}),
+                                [56] MemRead, [57] MemWrite, [58] zeroExtendFlag, [59] memToReg, [61:60] RegWrite, [65:62]EXRegDest*/
+stage_buffer #(.SIZE(66)) IDEX (
+        .in({IFID_Output[11:8], SignalFlushMuxOutput, rd1SrcMuxOutput, rd2SrcMuxOutput, signExtendedImmediate, IFID_Output[3:0]}),
         .writeEnable(1'b1),             
         .clk(clk),
         .flush(reset_n),
@@ -163,10 +172,79 @@ stage_buffer #(.SIZE(62)) IDEX (
 
 //Instantiation of EX Stage
 mux2to1 ALUSource1Mux (
-        .switch(aluBType),                      
-        .input1(IFAdderOutput),
-        .input2(IDAdderOutput),
-        .out(PCSourceMuxOutput)
+        .switch(IDEX_Output[52]),                      
+        .input1(IDEX_Output[51:36]),
+        .input2(IDEX_Output[19:4]),
+        .out(ALUSource1MuxOutput)
 );
+
+mux2to1 ALUSource2Mux (
+        .switch(IDEX_Output[53]),                      
+        .input1(IDEX_Output[35:20]),
+        .input2(IDEX_Output[19:4]),
+        .out(ALUSource2MuxOutput)
+);
+
+alu_control ALUControlUnit(
+        .aluControlOp(IDEX_Output[55:54]),
+        .functionCode(IDEX_Output[3:0]),
+        .ALUOp(aluOp)
+);
+
+alu ALU(
+    .A(ALUSource1MuxOutput),
+    .B(ALUSource2MuxOutput),
+    .CTRL(aluOp),
+    .result(ALUOutput),
+    .overflow(ALUOverflowOutput)
+);
+
+stage_buffer #(.SIZE(58)) EXM (
+        .in({IDEX_Output[61:60], IDEX_Output[59], IDEX_Output[58], IDEX_Output[57], IDEX_Output[56], ALUOutput, ALUOverflowOutput, IDEX_Output[51:36], IDEX_Output[65:62]}),
+        .writeEnable(1'b1),             
+        .clk(clk),
+        .flush(reset_n),
+        .out(IDEX_Output)       
+        );
+
+//Instantiation of M Stage
+dataMemory DataMemory(
+        .clk(clk),
+        .reset_n(reset_n),
+        .memoryWrite(EXM[53]),
+        .memoryRead(EXM[52]),
+        .address(EXM[51:36]),
+        .dataWrite(EXM[19:4]),
+        .dataRead(DataMemoryOutput)
+);
+
+zero_extend ZeroExtend(
+        .lowerByteDataMemoryOutput(DataMemoryOutput[7:0]),
+        .zeroExtendedResult(zeroExtendedResult)
+);
+
+mux2to1 DataMemoryOutputMux(
+        .switch(EXM[54]),                      
+        .input1(DataMemoryOutput),
+        .input2(zeroExtendedResult),
+        .out(DataMemoryOutputMuxResult)
+);
+
+stage_buffer #(.SIZE()) MWB (
+        .in({ EXM[57:56], EXM[55], EXM[51:36], DataMemoryOutputMuxResult, EXM[35:20], EXM[3:0]}),
+        .writeEnable(1'b1),             
+        .clk(clk),
+        .flush(reset_n),
+        .out(MWB_Output)       
+        );
+
+//Instantiation of WB Stage
+mux2to1 MemToRegMux(
+        .switch(MWB[52]),                      
+        .input1(MWB[35:20]),
+        .input2(MWB[51:36]),
+        .out(MemToRegMuxOutput)
+);
+
 
 endmodule
