479fe1dd7acb31e5c497dfca8068ba813210bf96
Daniel Olsen <dolsen@csus.edu>
2021-05-12 06:31:41 -0700

    all modules added besides HDU and FU

--- cpu.v
+++ cpu.v
@@ -24,19 +24,17 @@ module cpu(
     input clk, reset_n, 
     output PCSrc, aluBType, aluSrc, zeroExtendFlag, memRead, memWrite, memToReg, 
     output [1:0] aluControlOp, regWrite, 
-    output [2:0] jumpBranch,
+    output [2:0] jumpBranch, aluOp,
     output [15:0] PCOutput, IFAdderOutput, Instruction, rd1Read, rd2Read, reg0Read, signExtendedImmediate, ShiftResult, IDAdderOutput,
-    output [31:0] IFID_Output /*[31:16] IFAdderOutput, [15:0] Instruction Output*/
+    output [31:0] IFID_Output, /*[31:16] IFAdderOutput, [15:0] Instruction Output*/
     output [65:0] IDEX_Output, /*[3:0] Function Code, [19:4] signExtendedImmediate, [35:20] rd2SrcMuxOutput, [51:36]rd1SrcMuxOutput, [52] ALUBType, [53] ALUSrc, [55:54] ALUControlOp}
                                 [56] MemRead, [57] MemWrite, [58] zeroExtendFlag, [59] memToReg, [60:61] RegWrite, [65:62] EXRegDst*/
     output [57:0] EXM_Output, /*[3:0] MRegDest, [19:4] DataToWrite, [35:20] ALUOverflowOutput, [51:36] ALUOutput, [52] MemRead, [53] MemWrite, [54] zeroExtendFlag, [55] memToReg, [57:56] RegWrite*/
-    output [55:0] MWB_Output /* [3:0] WBRegDest, [19:4] ALUOverflowOutput, [35:20] DataMemoryOutputMuxResult, [51:36] ALUOutput, [52] memToReg, [54:53]RegWrite*/
+    output [54:0] MWB_Output /* [3:0] WBRegDest, [19:4] ALUOverflowOutput, [35:20] DataMemoryOutputMuxResult, [51:36] ALUOutput, [52] memToReg, [54:53]RegWrite*/
 );
 
-.in({ EXM[57:56], EXM[55], EXM[51:36], DataMemoryOutputMuxResult, EXM[35:20], EXM[3:0]}),
 
-
-wire [15:0] PCSourceMuxOutput, ALUSource1MuxOutput, ALUSource2MuxOutput, exReg0, ALUOverflowOutput, ALUOutput, DataMemoryOutput, zeroExtendedResult, MemToRegMuxOutput;
+wire [15:0] PCSourceMuxOutput, ALUSource1MuxOutput, ALUSource2MuxOutput, exReg0, ALUOverflowOutput, ALUOutput, DataMemoryOutput, zeroExtendedResult, MemToRegMuxOutput, rd1SrcMuxOutput, rd2SrcMuxOutput, DataMemoryOutputMuxResult;
 wire [9:0] SignalFlushMuxOutput;
 
 //Instantiation of IF Stage
@@ -82,38 +80,40 @@ register RegisterFile(
         .reset_n(reset_n), 
         .registerRead1(IFID_Output[11:8]),
         .registerRead2(IFID_Output[7:4]),
-        .regWriteLocal(4'hE),                   //Actual value added later
-        .registerWrite(2'b11),               //Actual value added later
-        .dataWrite(16'h0000),               //Actual value added later 
-        .r0Write(16'h0000),                 //Actual value added later
+        .regWriteLocal(MWB_Output[3:0]),                   //New
+        .registerWrite(MWB_Output[54:53]),                 //New
+        .dataWrite(MemToRegMuxOutput),              //New 
+        .r0Write(MWB_Output[19:4]),                        //New
         .dataRead1(rd1Read),
         .dataRead2(rd2Read), 
         .r0Read(reg0Read)
         );
 
 mux4to1 Register0SourceMux(
-        .switch(2'b00),                      //Add later, output of OFrwarding UNnit (R0Fwd)
+        .switch(2'b00),                             //Add later, output of OFrwarding UNnit (R0Fwd)
         .input1(reg0Read),
         .input2(ALUOverflowOutput),  
-        .input3(16'h0000),                  //Add later. m Stage R OUTPUT
-        .input4(16'h0000),                   //Add later, WB STAGE R OUTPUT
+        .input3(EXM_Output[35:20]),                        //M Stage R OUTPUT
+        .input4(MWB_Output[19:4]),                         //WB STAGE R OUTPUT
         .out(reg0Read)
 );
 
 mux4to1 rd1SourceMux(
-        .switch(2'b00),                      //Add later, output of OFrwarding UNnit (Op1Fwd)
+        .switch(2'b00),                            //Add later, output of OFrwarding UNnit (Op1Fwd)
         .input1(reg0Read),
         .input2(ALUOutput), 
-        .input3(16'h0000),                  //Add later. m Stage ALU OUTPUT
-        .input4(16'h0000)                   //Add later, WB STAGE ALU OUTPUT
+        .input3(EXM_Output[51:36]),                       //M Stage ALU OUTPUT
+        .input4(MemToRegMuxOutput),                 //WB STAGE ALU OUTPUT
+        .out(rd1SrcMuxOutput)
 );
 
 mux4to1 rd2SourceMux(
-        .switch(2'b00),                      //Add later, output of OFrwarding UNnit (Op2Fwd)
+        .switch(2'b00),                            //Add later, output of OFrwarding UNnit (Op2Fwd)
         .input1(reg0Read),
-        .input2(16'h0000),                  //Add later. EX STAGE ALU OUTPUT
-        .input3(16'h0000),                  //Add later. m Stage ALU OUTPUT
-        .input4(16'h0000)                   //Add later, WB STAGE ALU OUTPUT
+        .input2(ALUOutput),                        //EX STAGE ALU OUTPUT
+        .input3(EXM_Output[51:36]),                       //M Stage ALU OUTPUT
+        .input4(MemToRegMuxOutput),                 //WB STAGE ALU OUTPUT
+        .out(rd2SrcMuxOutput)
 );
 
 comparator Comparator(
@@ -188,7 +188,7 @@ mux2to1 ALUSource2Mux (
 alu_control ALUControlUnit(
         .aluControlOp(IDEX_Output[55:54]),
         .functionCode(IDEX_Output[3:0]),
-        .ALUOp(aluOp)
+        .aluOp(aluOp)
 );
 
 alu ALU(
@@ -199,22 +199,24 @@ alu ALU(
     .overflow(ALUOverflowOutput)
 );
 
+
+/*[3:0] MRegDest, [19:4] DataToWrite, [35:20] ALUOverflowOutput, [51:36] ALUOutput, [52] MemRead, [53] MemWrite, [54] zeroExtendFlag, [55] memToReg, [57:56] RegWrite*/
 stage_buffer #(.SIZE(58)) EXM (
         .in({IDEX_Output[61:60], IDEX_Output[59], IDEX_Output[58], IDEX_Output[57], IDEX_Output[56], ALUOutput, ALUOverflowOutput, IDEX_Output[51:36], IDEX_Output[65:62]}),
         .writeEnable(1'b1),             
         .clk(clk),
         .flush(reset_n),
-        .out(IDEX_Output)       
+        .out(EXM_Output)       
         );
 
 //Instantiation of M Stage
 dataMemory DataMemory(
         .clk(clk),
         .reset_n(reset_n),
-        .memoryWrite(EXM[53]),
-        .memoryRead(EXM[52]),
-        .address(EXM[51:36]),
-        .dataWrite(EXM[19:4]),
+        .memoryWrite(EXM_Output[53]),
+        .memoryRead(EXM_Output[52]),
+        .address(EXM_Output[51:36]),
+        .dataWrite(EXM_Output[19:4]),
         .dataRead(DataMemoryOutput)
 );
 
@@ -224,14 +226,14 @@ zero_extend ZeroExtend(
 );
 
 mux2to1 DataMemoryOutputMux(
-        .switch(EXM[54]),                      
+        .switch(EXM_Output[54]),                      
         .input1(DataMemoryOutput),
         .input2(zeroExtendedResult),
         .out(DataMemoryOutputMuxResult)
 );
 
-stage_buffer #(.SIZE()) MWB (
-        .in({ EXM[57:56], EXM[55], EXM[51:36], DataMemoryOutputMuxResult, EXM[35:20], EXM[3:0]}),
+stage_buffer #(.SIZE(55)) MWB (
+        .in({ EXM_Output[57:56], EXM_Output[55], EXM_Output[51:36], DataMemoryOutputMuxResult, EXM_Output[35:20], EXM_Output[3:0]}),
         .writeEnable(1'b1),             
         .clk(clk),
         .flush(reset_n),
@@ -240,9 +242,9 @@ stage_buffer #(.SIZE()) MWB (
 
 //Instantiation of WB Stage
 mux2to1 MemToRegMux(
-        .switch(MWB[52]),                      
-        .input1(MWB[35:20]),
-        .input2(MWB[51:36]),
+        .switch(MWB_Output[52]),                      
+        .input1(MWB_Output[35:20]),
+        .input2(MWB_Output[51:36]),
         .out(MemToRegMuxOutput)
 );
 
--- cpu_fixture.v
+++ cpu_fixture.v
@@ -4,17 +4,27 @@
 module cpu_fiixture;
 
 reg clk, reset_n;
-wire [15:0] PCOutput, IFAdderOutput, Instruction;
+wire [15:0] PCOutput, IFAdderOutput, Instruction, rd1Read, rd2Read, reg0Read;
 wire [31:0] IFID_Output;
 
 
 initial
 	$vcdpluson;
 
-initial
+/*initial
+begin
+    $display("IF OUTPUTS---------------------------------------------------------------------------------------------------------------------------------------------");
 	$monitor($time, "  clk = %b    reset_n = %b    PCOutput = %h     IFAdderOutput: %h  Instruction:  %h   IFID_Output: %h", clk, reset_n, PCOutput, IFAdderOutput, Instruction, IFID_Output);
+end*/
 
-cpu cpu_test(.clk(clk), .reset_n(reset_n), .PCOutput(PCOutput), .IFAdderOutput(IFAdderOutput), .Instruction(Instruction), .IFID_Output(IFID_Output));
+initial
+begin
+    $display("ID OUTPUTS---------------------------------------------------------------------------------------------------------------------------------------------");
+	$monitor($time, "  IFID_Output: %h\trd1Read: %h\trd2Read: %h\treg0Read: %h", IFID_Output, rd1Read, rd2Read, reg0Read);
+end
+
+
+cpu cpu_test(.clk(clk), .reset_n(reset_n), .PCOutput(PCOutput), .IFAdderOutput(IFAdderOutput), .Instruction(Instruction), .IFID_Output(IFID_Output), .rd1Read(rd1Read), .rd2Read(rd2Read), .reg0Read(reg0Read));
 
 initial
 begin
