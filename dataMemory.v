cae2c869aac50135530dc389dbf63e324a7d5c9d
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2021-05-13 15:06:59 -0700

    updated

--- dataMemory.v
+++ dataMemory.v
@@ -4,17 +4,23 @@
  * Name: dataMemory
  */
 
+/*
+ * Date 05-04-2021
+ * Author: Ramsey Alahmad
+ * Name: dataMemory
+ */
+
 
 module dataMemory
 (
-    input clk, reset_n, memoryWrite, memoryRead,
-    input [7:0] address,
+    input clk, reset_n, memoryWrite, memoryRead, sb,
+    input [15:0] address,
     input [15:0] dataWrite,
     output reg [15:0] dataRead
 );
 
 
-reg [7:0] memoryData [255:0];
+reg [7:0] memoryData [65535:0];
 
 integer i;
 
@@ -32,21 +38,24 @@ begin
         memoryData [7] = 8'hBE;
         memoryData [8] = 8'hEF;
         memoryData [9] = 8'hAD;
-        dataRead <= {memoryData[1], memoryData[0]};
-        for(i = 10; i <= 255; i = i+1) // sets all other addresses to be 0 
+        for(i = 10; i <= 65535; i = i+1) // sets all other addresses to be 0 
             memoryData[i] <= 8'h00;
     end
-    else if(memoryWrite) // concatenation 
-        {memoryData[address], memoryData[address+1]} <= dataWrite;
-    
-    // if(memoryRead) // takes the concatenation and places them in data read 
-    //     dataRead <= {memoryData[address], memoryData[address+1]};
-end
+
+    else if(memoryWrite) // concatenation
+      begin
+        if(!sb)
+                {memoryData[address+1], memoryData[address]} <= dataWrite;
+        else
+                memoryData[address] <= dataWrite[7:0];
+      end
+   end
 
 always@(*)
 begin
-    if(memoryRead) // takes the concatenation and places them in data read 
-        dataRead = {memoryData[address], memoryData[address+1]};
+    if(memoryRead) // takes the concatenation andp laces them i nthe data read
+            dataRead = {memoryData[address+1], memoryData[address]};
+    else
+            dataRead = 16'h0000;
 end
-
-endmodule
+endmodule 
